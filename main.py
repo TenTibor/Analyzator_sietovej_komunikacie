ec308cabf50c245ea811f3a436c54f18beaf63d4
TenTibor <tibqor@gmail.com>
2021-10-17 14:41:09 +0200

    Uloha2

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+from ast import literal_eval
+
 from scapy.all import *
 
-# load one vzorku
-data = rdpcap('vzorky/trace-2.pcap')
+# load files and create db
+data = rdpcap('vzorky/eth-9.pcap')
+file = open('db.txt', "r")
+protocols = []
+for iProtocol in file:
+    protocols.append(iProtocol.split(" "))
 
 for index, packet in enumerate(data):
     rawPacket = raw(packet)
@@ -30,10 +36,44 @@ for index, packet in enumerate(data):
 
     # packet ether type
     packetEtherType = hexPacket[24:28]
+    protocol = hexPacket[24:28]
 
     # get product type
     if packetEtherType > "05DC":
+        # Ethernet
         packetType = "Ethernet II"
+        for xProtocol in protocols:
+            if xProtocol[0] == protocol:
+                protocol = xProtocol[1].replace("\n", "")
+        lengthHead = int(hexPacket[29]) * 4  # why tf 4?
+
+        # Calculate source IP adress
+        endOfHead = 32 + lengthHead
+        sourceIpAddressHex = hexPacket[endOfHead:endOfHead + 8]
+        sourceIpAddress = ""
+        i = 0
+        while i < len(sourceIpAddressHex):
+            sourceIpAddress += str(int(sourceIpAddressHex[i] + sourceIpAddressHex[i + 1], 16))
+            if i + 2 != len(sourceIpAddressHex):
+                sourceIpAddress += ":"
+            i += 2
+
+        # Calculate destination IP adress
+        destinationIpAddressHex = hexPacket[endOfHead + 8:endOfHead + 16]
+        destinationIpAddress = ""
+        i = 0
+        while i < len(destinationIpAddressHex):
+            destinationIpAddress += str(int(destinationIpAddressHex[i] + destinationIpAddressHex[i + 1], 16))
+            if i + 2 != len(destinationIpAddressHex):
+                destinationIpAddress += ":"
+            i += 2
+
+        # get protocol
+        ipvProtocol = hexPacket[46:48]
+        for xProtocol in protocols:
+            if xProtocol[0] == ipvProtocol or xProtocol[1] == ipvProtocol:
+                ipvProtocol = xProtocol[2].replace("\n", "")
+
     else:
         # If it is not ethernet II, get another B to check
         packetHexForType = hexPacket[28:30]
@@ -45,12 +85,21 @@ for index, packet in enumerate(data):
             packetType = "IEEE 802.3 - LLC"
 
     # print all data
-    print(f"Rámec: {index+1}")
+    print(f"Rámec: {index + 1}")
     print(f"Dĺžka rámca poskytnutá pcap API - {lengthPacket} B")
     print(f"Dĺžka rámca prenášaného po médiu - {mediumLength} B")
     print(f"Typ: {packetType}")
+    print(f"Protocol: {protocol}")
+    print(f"Zdrojová IP adresa: {sourceIpAddress}")
+    print(f"Cieľová IP adresa: {destinationIpAddress}")
     print(f"Zdrojová MAC adresa: {sourceMacAddress}")
     print(f"Cieľová MAC adresa: {destinationMacAddress}")
+
+    # print ports
+    print(ipvProtocol)
+    if ipvProtocol == "TCP" or ipvProtocol == "UDP":
+        print("Source port: " + str(int(hexPacket[endOfHead + 16:endOfHead + 20], 16)))
+        print("Destination port: " + str(int(hexPacket[endOfHead + 20:endOfHead + 24], 16)))
 
     # print hex packet
     for index, char in enumerate(hexPacket):
