7c9db8c6dbfdda3b747cecb74ab7bdda057b984a
TenTibor <tibqor@gmail.com>
2021-10-19 13:02:04 +0200

    Frame object

---
+++ frame.py
@@ -0,0 +1,158 @@
+from scapy.all import *
+
+
+class Frame:
+    db_protocols = None
+    index = None
+    hexPacket = None
+    lengthPacket = None
+    mediumLength = None
+
+    destinationMacAddress = ""
+    sourceMacAddress = ""
+
+    sourceIpAddress = ""
+    destinationIpAddress = ""
+
+    endOfHead = None
+    packetType = None
+    protocol = None
+    ipvProtocol = None
+
+    sourcePort = None
+    destinationPort = None
+
+    def __init__(self, rawPacket, index, protocols):
+        self.get_hex_from_raw(rawPacket)
+        self.index = index
+        self.db_protocols = protocols
+
+        # calc everything
+        self.calc_frame()
+
+    def get_hex_from_raw(self, rawPacket):
+        rawPacket = raw(rawPacket)
+        self.hexPacket = rawPacket.hex()
+
+    def print_hex(self):
+        print("")
+        for index, char in enumerate(self.hexPacket):
+            print(char, end="")
+            if index % 2:
+                print(" ", end="")
+            if index % 16 == 15:
+                print(" ", end="")
+            if index % 32 == 31:
+                print("")
+
+    def calculate_length(self):
+        self.lengthPacket = int(len(self.hexPacket) / 2)
+        mediumLength = self.lengthPacket + 4
+        if mediumLength < 64:
+            mediumLength = 64
+        self.mediumLength = mediumLength
+
+    def get_mac_addresses(self):
+        # destination MAC address
+        for b in range(0, 12):
+            self.destinationMacAddress += self.hexPacket[b]
+            if b % 2 != 0 and b != 11:
+                self.destinationMacAddress += ":"
+
+        # source MAC address
+        for b in range(12, 24):
+            self.sourceMacAddress += self.hexPacket[b]
+            if b % 2 != 0 and b != 23:
+                self.sourceMacAddress += ":"
+
+    def print_frame(self):
+        print(f"Rámec: {self.index}")
+        print(f"PCAP API packet length: {self.lengthPacket}B")
+        print(f"Real packet length: {self.mediumLength}B")
+        print(self.packetType)
+        print(f" -{self.protocol}")
+        if self.sourceIpAddress:
+            print(f" -Zdrojová IP adresa: {self.sourceIpAddress}")
+            print(f" -Cieľová IP adresa: {self.destinationIpAddress}")
+        print(f" -Zdrojová MAC adresa: {self.sourceMacAddress}")
+        print(f" -Cieľová MAC adresa: {self.destinationMacAddress}")
+
+        # print ports
+        if self.ipvProtocol:
+            if self.ipvProtocol == "TCP" or self.ipvProtocol == "UDP":
+                print(self.ipvProtocol)
+                sourcePort = int(self.hexPacket[self.endOfHead + 16:self.endOfHead + 20], 16)
+
+                # print protocol by source port
+                for xProtocol in self.db_protocols[9:]:
+                    # print(xProtocol[1], sourcePort)
+                    if int(xProtocol[1]) == sourcePort:
+                        print("- " + xProtocol[2].replace("\n", ""))
+
+                # print source and destination port
+                print(" -Source port: " + str(self.sourcePort))
+                print(" -Destination port: " + str(int(self.hexPacket[self.endOfHead + 20:self.endOfHead + 24], 16)))
+        self.print_hex()
+        print("\n------------------------------------------------")
+
+    def calc_ethernet(self):
+        self.packetType = "Ethernet II"
+        self.protocol = self.hexPacket[24:28]
+
+        for xProtocol in self.db_protocols:
+            if xProtocol[0] == self.protocol:
+                self.protocol = xProtocol[1].replace("\n", "")
+        lengthHead = int(self.hexPacket[29]) * 4
+
+        # Calculate source IP address
+        self.endOfHead = 32 + lengthHead
+        sourceIpAddressHex = self.hexPacket[self.endOfHead:self.endOfHead + 8]
+        i = 0
+        while i < len(sourceIpAddressHex):
+            self.sourceIpAddress += str(int(sourceIpAddressHex[i] + sourceIpAddressHex[i + 1], 16))
+            if i + 2 != len(sourceIpAddressHex):
+                self.sourceIpAddress += ":"
+            i += 2
+
+        # Calculate destination IP address
+        destinationIpAddressHex = self.hexPacket[self.endOfHead + 8:self.endOfHead + 16]
+        i = 0
+        while i < len(destinationIpAddressHex):
+            self.destinationIpAddress += str(int(destinationIpAddressHex[i] + destinationIpAddressHex[i + 1], 16))
+            if i + 2 != len(destinationIpAddressHex):
+                self.destinationIpAddress += ":"
+            i += 2
+
+        # get UDP, TCP, ..
+        self.ipvProtocol = self.hexPacket[46:48]
+        for xProtocol in self.db_protocols[7:]:
+            if xProtocol[0] == self.ipvProtocol or xProtocol[1] == self.ipvProtocol:
+                self.ipvProtocol = xProtocol[2].replace("\n", "")
+
+    def calc_ieee(self):
+        # If it is not ethernet II, get another B to check
+        packetHexForType = self.hexPacket[28:30]
+        self.packetType = "IEEE 802.3"
+
+        self.protocol = "LLC"
+        for xProtocol in self.db_protocols[3:]:
+            # print(xProtocol[1], sourcePort)
+            if xProtocol[0] == packetHexForType:
+                protocol = xProtocol[1].replace("\n", "")
+
+    def calc_frame(self):
+        # length calculation
+        self.calculate_length()
+
+        # mac addresses calculation
+        self.get_mac_addresses()
+
+        # calc packet ether type
+        packetEtherTypeHex = self.hexPacket[24:28]
+
+        # get product type
+        if packetEtherTypeHex > "05DC":
+            # Ethernet
+            self.calc_ethernet()
+        else:
+            self.calc_ieee()
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from scapy.all import *
 
+from frame import Frame
+
 # load files and create db
 file = "trace-5.pcap"
 data = rdpcap('vzorky/' + file)
@@ -10,132 +12,6 @@ for iProtocol in file:
     protocols.append(iProtocol.split(" "))
 
 
-def print_hex(hexPacket):
-    print("")
-    for index, char in enumerate(hexPacket):
-        print(char, end="")
-        if index % 2:
-            print(" ", end="")
-        if index % 16 == 15:
-            print(" ", end="")
-        if index % 32 == 31:
-            print("")
-
-
-def print_frame(index, packet):
-    sourceIpAddress = None
-    ipvProtocol = None
-
-    rawPacket = raw(packet)
-    hexPacket = rawPacket.hex()
-
-    # length calculation
-    lengthPacket = int(len(hexPacket) / 2)
-    mediumLength = lengthPacket + 4
-    if mediumLength < 64:
-        mediumLength = 64
-
-    # going through parts of packet
-    # destination mac address
-    destinationMacAddress = ""
-    for b in range(0, 12):
-        destinationMacAddress += hexPacket[b]
-        if b % 2 != 0 and b != 11:
-            destinationMacAddress += ":"
-
-    # destination source address
-    sourceMacAddress = ""
-    for b in range(12, 24):
-        sourceMacAddress += hexPacket[b]
-        if b % 2 != 0 and b != 23:
-            sourceMacAddress += ":"
-
-    # packet ether type
-    packetEtherType = hexPacket[24:28]
-    protocol = hexPacket[24:28]
-
-    # get product type
-    if packetEtherType > "05DC":
-        # Ethernet
-        packetType = "Ethernet II"
-        for xProtocol in protocols:
-            if xProtocol[0] == protocol:
-                protocol = xProtocol[1].replace("\n", "")
-        lengthHead = int(hexPacket[29]) * 4
-
-        # Calculate source IP address
-        endOfHead = 32 + lengthHead
-        sourceIpAddressHex = hexPacket[endOfHead:endOfHead + 8]
-        sourceIpAddress = ""
-        i = 0
-        while i < len(sourceIpAddressHex):
-            sourceIpAddress += str(int(sourceIpAddressHex[i] + sourceIpAddressHex[i + 1], 16))
-            if i + 2 != len(sourceIpAddressHex):
-                sourceIpAddress += ":"
-            i += 2
-
-        # Calculate destination IP address
-        destinationIpAddressHex = hexPacket[endOfHead + 8:endOfHead + 16]
-        destinationIpAddress = ""
-        i = 0
-        while i < len(destinationIpAddressHex):
-            destinationIpAddress += str(int(destinationIpAddressHex[i] + destinationIpAddressHex[i + 1], 16))
-            if i + 2 != len(destinationIpAddressHex):
-                destinationIpAddress += ":"
-            i += 2
-
-        # get UDP, TCP, ..
-        ipvProtocol = hexPacket[46:48]
-        for xProtocol in protocols[7:]:
-            if xProtocol[0] == ipvProtocol or xProtocol[1] == ipvProtocol:
-                ipvProtocol = xProtocol[2].replace("\n", "")
-
-    else:
-        # If it is not ethernet II, get another B to check
-        packetHexForType = hexPacket[28:30]
-        packetType = "IEEE 802.3"
-
-        protocol = "LLC"
-        for xProtocol in protocols[3:]:
-            # print(xProtocol[1], sourcePort)
-            if xProtocol[0] == packetHexForType:
-                protocol = xProtocol[1].replace("\n", "")
-
-    # print all data
-    print(f"Rámec: {index + 1}")
-    print(f"PCAP API packet length: {lengthPacket}B")
-    print(f"Real packet length: {mediumLength}B")
-    print(packetType)
-    print(f" -{protocol}")
-    if sourceIpAddress:
-        print(f" -Zdrojová IP adresa: {sourceIpAddress}")
-        print(f" -Cieľová IP adresa: {destinationIpAddress}")
-    print(f" -Zdrojová MAC adresa: {sourceMacAddress}")
-    print(f" -Cieľová MAC adresa: {destinationMacAddress}")
-
-    # print ports
-    if ipvProtocol:
-        if ipvProtocol == "TCP" or ipvProtocol == "UDP":
-            print(ipvProtocol)
-            sourcePort = int(hexPacket[endOfHead + 16:endOfHead + 20], 16)
-
-            # print protocol by source port
-            for xProtocol in protocols[9:]:
-                # print(xProtocol[1], sourcePort)
-                if int(xProtocol[1]) == sourcePort:
-                    print("- " + xProtocol[2].replace("\n", ""))
-
-            # print source and destination port
-            print(" -Source port: " + str(sourcePort))
-            print(" -Destination port: " + str(int(hexPacket[endOfHead + 20:endOfHead + 24], 16)))
-
-    # print hex packet
-    print_hex(hexPacket)
-    print("\n------------------------------------------------")
-
-    return sourceIpAddress
-
-
 def tftp_communications():
     pass
 
@@ -143,7 +19,15 @@ def tftp_communications():
 def all_frames():
     allEthernetNodes = []
     for index, packet in enumerate(data):
-        sourceIpAddress = print_frame(index, packet)
+
+        # get frame
+        this_frame = Frame(packet, index + 1, protocols)
+
+        # print frame
+        this_frame.print_frame()
+
+        # get source ip from frame
+        sourceIpAddress = this_frame.sourceIpAddress
 
         # Add source address to array if not exist. If yes.. Increase number of usage
         found = False
